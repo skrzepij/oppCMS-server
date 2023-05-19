28c9d1a1aceb64aaed8e5da3926b589efff5b002
tomasz.piotrowicz <pantomash@protonmail.com>
2023-05-19 13:14:57 +0200

    add missing prettier params and update .gitIgnore file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigModule } from "@nestjs/config";
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [
-      // Nest don't handle environment variables from default. We have to declare .env files in line below in order.
-      ConfigModule.forRoot({envFilePath: ['.env.local', '.env']}),
-      UsersModule,
-      TypeOrmModule.forRoot(
-        { type: "postgres", host: process.env.DATABASE_HOST, port: parseInt(process.env.DATABASE_PORT), username: process.env.DATABASE_USER, password: process.env.DATABASE_PASSWORD, database: process.env.DATABASE_NAME, synchronize: true, entities: ["**/*.entity.js"] }
-      )],
+    // Nest don't handle environment variables from default. We have to declare .env files in line below in order.
+    ConfigModule.forRoot({ envFilePath: ['.env.local', '.env'] }),
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      synchronize: true,
+      entities: ['**/*.entity.js'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,3 @@
 export class CreateUserDto {
-    name: string
+  name: string
 }
